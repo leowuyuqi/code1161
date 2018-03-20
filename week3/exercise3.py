a544afd1fc3dc87ad28d1da29a517ed47f0d261b
leowuyuqi <347345277@qq.com>
2018-03-20 17:48:58 +1100

    work

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -29,6 +29,51 @@ def advancedGuessingGame():
     purpose if you can!
     """
     return "You got it!"
+def advancedGuessingGame():
+  print ("Welcome to the guessing game!")
+  i=input("Set a lower bound: ")
+  while True:
+    try:
+        i= int(i)
+        break
+    except ValueError:
+        i=input('Set a lower bound again: ')
+        continue
+  s=input("Set a higher bound: ")
+  while True:
+    try:
+        s = int(s)
+        while True:
+            if int(s)>int(i):
+              break
+            else:
+              s=input("The number should bigger than the low bound, set again: ")
+        break
+    except ValueError:
+        s=input('Set a higher bound again: ')
+        continue
+  print ("Now, guess a number between "+str(i)+" and "+str(s))
+  actualNumber = random.randint(int(i), int(s))
+  guessed = False
+  while not guessed:
+    guessedNumber = input("guess a number: ")
+    while True:
+        try: 
+            guessedNumber=int(guessedNumber)
+            break
+        except ValueError:
+            guessedNumber=input('You should enter a number to guess: ')
+            continue
+    print("you guessed {},".format(guessedNumber),)
+    if int(guessedNumber) == int(actualNumber):
+        print("you got it!! It was {}".format(actualNumber))
+        guessed = True
+    elif int(guessedNumber) < int(actualNumber):
+        print("too small, try again ")
+    else:
+        print("too big, try again  ")  
+    return "You got it!"
+
 
 
 if __name__ == "__main__":
